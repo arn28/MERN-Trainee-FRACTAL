268e0d8394e2128a2e55743ca451843e6166597a
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-01-19 13:11:36 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,43 +27,11 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: 'true' }))
 
-
-
-
-
-
-app.use('/api/product.model', productRoute)
-
+app.use('/api/product', productRoute)
 
 app.get('/', (req, res) => {
     res.end('Welcome to backend Node.js to MERN Trainee FRACTAL. Running...')
 })
-
-// const whitelist = ['http://localhost:8080','http://localhost:3000', 'https://mern-trainee-fractal-frontend.vercel.app/', 'https://mern-trainee-fractal-frontend-arn28.vercel.app/', 'https://mern-trainee-fractal-frontend-git-main-arn28.vercel.app/', 'https://arn28.vercel.app/'];
-
-// const options = {
-//     origin: (origin, callback) => {
-//         if (whitelist.includes(origin) || !origin) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('no permitido'));
-//         }
-//     }
-// }
-// app.use(cors(options));
-
-
-// app.use(cors({
-//     origin: '*'
-// }));
-
-
-
-//Basic server config
-// const port = 9001;
-// app.listen(port, function () {
-//     console.log('NODE server is running correctly ...')
-// })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
